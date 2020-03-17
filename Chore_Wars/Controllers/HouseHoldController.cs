91d79cbd3a2bef8c969354bac35d98ee3e8ae1f0
SRCummings1 <45797938+SRCummings1@users.noreply.github.com>
2020-03-17 13:37:44 -0400

    update commit

---
+++ Chore_Wars/Controllers/HouseHoldController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chore_Wars.Controllers
+{
+    public class HouseHoldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoginHouseHold()
+        {
+            return View();
+        }
+        public IActionResult RegisterHouseHold()
+        {
+            return View();
+        }
+        public IActionResult ViewHouseHoldMembers()
+        {
+            return View();
+        }
+
+        public IActionResult ViewHouseHoldChores()
+        {
+            return View();
+        }  
+    }
+}
+//LoginHousehold() <- Identity(mostly)
+//RegisterHouseHold() <- Enter household name(‘The Cooper Family’)
+//ViewHouseHoldMembers()
+//ViewHouseHoldChores()
