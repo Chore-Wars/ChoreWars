1d1dba0635b796276b565eaddd72b06855cbce5b
SRCummings1 <45797938+SRCummings1@users.noreply.github.com>
2020-03-17 16:46:24 -0400

    end of day merge

--- Chore_Wars/Controllers/HouseHoldController.cs
+++ Chore_Wars/Controllers/HouseHoldController.cs
@@ -13,20 +13,32 @@ namespace Chore_Wars.Controllers
             return View();
         }
 
-        public IActionResult LoginHouseHold()
-        {
-            return View();
-        }
-        public IActionResult RegisterHouseHold()
-        {
-            return View();
-        }
-        public IActionResult ViewHouseHoldMembers()
+        
+        public IActionResult RegisterHouseHold(int Id)
         {
             return View();
         }
 
-        public IActionResult ViewHouseHoldChores()
+        //present a list of members that exist inside the household
+        //each household represented by the ASP Net user ID
+        //Create a list "Players-where the ID" bundle list of users and display list of household players
+        public IActionResult ViewHouseHoldMembers(String Member)
+        {
+
+            
+            //view will have list of users
+            //each user can have a button action to sign in with, each one a form with name/household/submit button goes to an action "Login HouseHoldMemmber"  
+            //set up session, session value == to playerID
+            //view all members, select with button, redirect to action, which difficulty lvl question, Session user gets points for correct answer or minues for incorrect.  
+            //return redirect to action-
+            return View();
+        } 
+        public IActionResult LoginPlayer()
+        {
+            return RedirectToAction("SelectQuestion", "Question");
+        }
+
+        public IActionResult ViewHouseHoldChores(string Chores)
         {
             return View();
         }  
