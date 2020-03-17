d0054c628d641901705ec38672ad2c78f1977f90
jamesjr19 <ruwartja@gmail.com>
2020-03-17 13:37:18 -0400

    Added 'AddChore' method

---
+++ Chore_Wars/Controllers/ChoreController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chore_Wars.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chore_Wars.Controllers
+{
+    public class ChoreController : Controller
+    {
+
+        private readonly ChoreWarsDbContext _context;
+        public ChoreController(ChoreWarsDbContext context)
+        {
+            _context = context;
+        }
+        //add chores to database
+        [HttpGet]
+        public IActionResult AddChore()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddChore(Chore newChore)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Add(newChore);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult BuyChoresFor(int userid)
+        {
+            return View();
+        }
+
+
+        public IActionResult ViewAssignedChores(int userid)
+        {
+            return View();
+        }
+
+
+        public IActionResult EditChore()
+        {
+            return View();
+        }
+
+        public IActionResult DeleteChore()
+        {
+            return View();
+        }
+    }
+}
