de03dca66a59de4213952671cd3858ac76ebe014
Kyle Vaughn <kcvaughn75@gmail.com>
2020-03-17 17:44:46 -0400

    added add player and delete player

---
+++ Chore_Wars/Controllers/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chore_Wars.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chore_Wars.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly ChoreWarsDbContext _context;
+
+        public PlayerController(ChoreWarsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult AddPlayer()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult AddPlayer(Player newPlayer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Player.Add(newPlayer);
+                _context.SaveChanges();
+            }
+            return View();
+        }
+
+        //public IActionResult DeletePlayer(int id)
+        //{
+        //    Player found = _context.Player.Find(id);
+        //    if (found != null)
+        //    {
+        //        List<Player> thisPlayer = _context.Player.Remove()
+        //        //List<Player> thisPlayer = _context.Player.Where(x => x.UserId == found.UserId).ToList();
+        //        if (thisPlayer != null)
+        //        {
+        //            foreach (Player player in thisPlayer)
+        //            {
+        //                player.UserId = null;
+        //            }
+        //        }
+
+        //        _context.Player.Remove(found);
+        //        _context.SaveChanges();
+        //    }
+        //    return RedirectToAction("Index");
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
